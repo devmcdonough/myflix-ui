dbe70d6a904173c696f1f3dfe407ee9b280ff449
devmcdonough <devmcdonough@gmail.com>
2024-12-03 15:47:01 -0500

    Updated movie view styling

--- src/components/views/movie-view/movie-view.jsx
+++ src/components/views/movie-view/movie-view.jsx
@@ -1,44 +1,30 @@
 import { useParams } from "react-router";
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import './movie-view.scss';
 import { FavoriteButton } from "../../shared/favorite-button/favorite-button";
+import "./movie-view.scss";
 
 export const MovieView = ({ movies, user, setUser, token }) => {
-
     const { movieId } = useParams();
 
-        const movie = movies.find((m) => m.id === movieId);
-        if (!movies) return <div>Movie not found</div>;
-
+    const movie = movies.find((m) => m.id === movieId);
+    if (!movies) return <div>Movie not found</div>;
 
     return (
-
-        <div className="container mt-4">
-            <Card className="mb-3" style={{ maxWidth: '640px' }}>
-                <Row className="g-0">
-                    <Col md={8}>
-                        <img src={movie.imagepath} className="img-fluid rounded-start" alt={movie.title} />
-                    </Col>
-                    <Col md={4}>
-                        <Card.Body>
-                            <Card.Title>{movie.title}</Card.Title>
-                            <Card.Text>Dir: {movie.director.Name}</Card.Text>
-                            <Card.Text>{movie.genre.Name}</Card.Text>
-                            <Card.Text>{movie.description}</Card.Text>
-                            <FavoriteButton movie={movie} user={user} setUser={setUser} token={token} />
-                            <Link to="/" className="btn btn-secondary">
-                                Back
-                            </Link>
-                        </Card.Body>
-                    </Col>
-                </Row>
-            </Card>
+        <div className="movie-view-container">
+            <Link to="/" className="back-button">← Back</Link>
+            <h1 className="movie-title">{movie.title}</h1>
+            <div className="movie-details">
+                <div className="movie-text">
+                    <p><span className="label">Genre:</span> <span className="value">{movie.genre.Name}</span></p>
+                    <p><span className="label">Director:</span> <span className="value">{movie.director.Name}</span></p>
+                    <p className="description">{movie.description}</p>
+                    <FavoriteButton movie={movie} user={user} setUser={setUser} token={token} />
+                </div>
+                <div className="movie-image-container">
+                    <img src={movie.imagepath} alt={movie.title} className="movie-image" />
+                </div>
+            </div>
         </div>
     );
 };
